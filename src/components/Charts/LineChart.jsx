ecc07830d0946438b2382432769461d83d42285a
Abdullah-Ansari1 <abd45015@gmail.com>
2022-09-06 16:06:39 +0500

    Initial Commit

---
+++ src/components/Charts/LineChart.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ChartComponent,SeriesCollectionDirective,LineSeries, SeriesDirective,Inject, DateTime,Legend,Tooltip } from '@syncfusion/ej2-react-charts';
+import { LinePrimaryXAxis,LinePrimaryYAxis,lineCustomSeries } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const LineChart = () => {
+  const {currentMode} = useStateContext();
+  return (
+    <ChartComponent
+    id="line-chart"
+    height='480px'
+    primaryXAxis={LinePrimaryXAxis}
+    primaryYAxis={LinePrimaryYAxis}
+    chartArea={{border:{width:0}}}
+    tooltip={{enable:true}}
+    background={currentMode === "Dark"? "#33373E": "#fff"}
+    legendSettings={{ background: 'white' }}
+    >
+      <Inject services={[LineSeries,DateTime,Legend,Tooltip]}/>
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item,index)=>
+        <SeriesDirective 
+        key={index}
+        {...item}
+        />
+        )}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  )
+}
+
+export default LineChart
---
+++ src/components/Charts/Stacked.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {ChartComponent, SeriesCollectionDirective,Inject,Legend,Category,StackingColumnSeries,Tooltip, SeriesDirective} from '@syncfusion/ej2-react-charts';
+import {stackedCustomSeries,stackedPrimaryXAxis,stackedPrimaryYAxis} from "../../data/dummy";
+const Stacked = ({width,height}) => {
+  return (
+    <ChartComponent 
+    width={width}
+    height={height}
+    id="charts"
+    primaryXAxis={stackedPrimaryXAxis}
+    primaryYAxis={stackedPrimaryYAxis}
+    chartArea={{border:{width:0}}}
+    tooltip={{enable:true}}
+    legendSettings={{background:"white"}}
+    >
+      <Inject
+      services={[Legend,Category,StackingColumnSeries,Tooltip]}
+      />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item,index)=>
+        <SeriesDirective key={index} {...item}/>)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  )
+}
+
+export default Stacked
